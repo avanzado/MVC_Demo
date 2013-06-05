3b870c870e1eb93684ff5ecb7064db90926a2d5d
Virender Maurya <virender.maurya@avanzadotechnologies.com>
2013-06-05 11:21:49 +0530

    Virendras Changes
    Virendrs Changes

---
+++ MVCDemoApplication/MVCDemoApplication/Controllers/AddController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCDemoApplication.Models;
+namespace MVCDemoApplication.Controllers
+{
+    public class AddController : Controller
+    {
+        //
+        // GET: /Add/
+        [HttpGet]
+        public ActionResult Add()
+        {
+        
+            if (Request.QueryString.Count>0)
+            {
+                if (Request.QueryString["page"] != null)
+                {
+                    var lst = PropertyMaster.GetList();
+                    return View("AddRecord", lst.AsEnumerable());
+
+                } 
+                else if (Request.QueryString["sort"] != null && Request.QueryString["sortdir"] != null)
+                {
+                    var lst = PropertyMaster.GetList(Request.QueryString["sort"].ToString(), Request.QueryString["sortdir"].ToString());
+                    return View("AddRecord", lst.AsEnumerable());
+
+                }
+            }
+
+         
+
+            //var PropertyList = PropertyMaster.GetList();
+            return View("");
+         
+        }
+        [HttpPost]
+        public ActionResult Add(PropertyMaster objPropertyMaster)
+        {
+           
+            if (ModelState.IsValid)
+            {
+                ModelState.Clear();
+                objPropertyMaster.AddData(objPropertyMaster);
+            }
+            else
+            {
+               
+            }
+
+            var lst = PropertyMaster.GetList();
+            return View("AddRecord", lst.AsEnumerable());
+        }
+       
+
+    }
+}
---
+++ MVCDemoApplication/MVCDemoApplication/Models/PropertyMaster.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace MVCDemoApplication.Models
+{
+    public class PropertyMaster
+    {
+        [Required(ErrorMessage = "Please enter Property Name")] 
+        public string PropertyName {get;set;}
+
+        [Required(ErrorMessage = "Please enter Property Description")] 
+        public string PropertyDescription { get; set; }
+
+
+        public static List<PropertyMaster> GetList(string sorton="", string sortdirection="")
+        {
+             List<PropertyMaster> _PropertyList = new List<PropertyMaster>();
+            string connectionString = ConfigurationManager.ConnectionStrings["DemoMVC_DB"].ConnectionString; ;// "Persist Security Info=False;User ID=sa;Password=sa@123;Initial Catalog=MVCDemoDB;Server=Bilal-PC";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand();
+                string sQuery = "Select PropertyName, PropertyDescription From PropertyMaster Order By LastModifiedDateTime Desc";
+                if (sorton.Length > 0 && sortdirection.Length > 0)
+                {
+                    sQuery = "Select PropertyName, PropertyDescription From PropertyMaster Order By " + sorton + " " + sortdirection;
+                }
+                cmd.CommandText = sQuery;
+                cmd.Connection = connection;
+                connection.Open();
+                SqlDataReader myDataReader;
+                myDataReader = cmd.ExecuteReader();
+                if (myDataReader.HasRows)
+                {
+                    while (myDataReader.Read())
+                    {
+                        PropertyMaster obj = new PropertyMaster();
+                        obj.PropertyName = myDataReader["PropertyName"].ToString();
+                        obj.PropertyDescription = myDataReader["PropertyDescription"].ToString();
+                        _PropertyList.Add(obj);
+                    }
+                }
+            }
+
+           
+            return _PropertyList;
+       }
+
+        public bool AddData(PropertyMaster objPropertyMaster)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["DemoMVC_DB"].ConnectionString; ;// "Persist Security Info=False;User ID=sa;Password=sa@123;Initial Catalog=MVCDemoDB;Server=Bilal-PC";
+
+          
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand();
+
+                cmd.CommandText = "INSERT INTO PropertyMaster (PropertyName, PropertyDescription) VALUES ('" + objPropertyMaster.PropertyName + "','" + objPropertyMaster.PropertyDescription + "')";
+               
+                cmd.Connection = connection;
+
+                connection.Open();
+                cmd.ExecuteNonQuery();
+            }
+            return true;
+        }
+        
+    }
+}
